11caf83bf53d529a34025c844faaebabbc871f3f
Harsh <harshagarwal81528@gmail.com>
2025-02-22 09:39:10 +0530

    project button and its functionality

--- frontend/src/Routes/AppRoutes.jsx
+++ frontend/src/Routes/AppRoutes.jsx
@@ -3,12 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react'
 import Login from '../Screens/Login';
 import Register from '../Screens/Register';
+import Home from '../Screens/Home';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>home</div>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="/profile" element={<div>profile</div>} />
