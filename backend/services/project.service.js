04d963805c9ee0369fdc18df76ddc9d47b093ea5
Harsh <harshagarwal81528@gmail.com>
2025-02-22 07:09:18 +0530

    user context and project model and service

---
+++ backend/services/project.service.js
@@ -0,0 +1,12 @@
+import Project from "../models/project.model";
+
+const createProject = async ({ name, userId }) => {
+    if (!name) {
+        throw new Error('Name is required');
+    }
+    if (!userId) {
+        throw new Error('User is required');
+    }
+    const project = await Project.create({ name, users: [userId] });
+    return project;
+}
